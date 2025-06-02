f887527490c7f45e41982c522ffebcb23abeb288
NozarLahooti <nozarlahooti@me.com>
2025-06-02 18:40:27 -0400

    Add API_KEY

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ document.getElementById('Calorie Number').addEventListener('click', () =>{
     }
 
 })
+
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch',
+  params: {
+    query: 'side salad',
+    diet: 'vegetarian',
+    intolerances: 'gluten',
+    includeIngredients: 'cheese,nuts',
+    excludeIngredients: 'eggs',
+    instructionsRequired: 'true',
+    fillIngredients: 'false',
+    addRecipeInformation: 'false',
+    addRecipeInstructions: 'false',
+    addRecipeNutrition: 'false',
+    maxReadyTime: '45',
+    ignorePantry: 'true',
+    sort: 'max-used-ingredients',
+    offset: '0',
+    number: '10'
+  },
+  headers: {
+    'x-rapidapi-key': 'API_KEY',
+    'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
