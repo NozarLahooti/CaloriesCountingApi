a56937f0d6663d6389d88a9e638ce933a4c4358e
NozarLahooti <nozarlahooti@me.com>
2025-06-02 23:28:26 -0400

    API Update

--- script.js
+++ script.js
@@ -3,38 +3,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const resultDiv = document.getElementById("result");
 
   button.addEventListener("click", async () => {
-    const food = document.getElementById("foodSearch").value.trim();
+    
+    const recipeName = document.getElementById("foodSearch").value.trim();
 
-    if (!food) {
-      resultDiv.innerText = "Please enter your food!";
+    if (!recipeName) {
+      resultDiv.innerText = "Please enter your recipe name!";
       return;
     }
 
+    
     const options = {
-      method: "GET",
-      url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
+      method: 'POST',
+      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/analyze',
       params: {
-        query: "side salad",
-        diet: "vegetarian",
-        intolerances: "gluten",
-        includeIngredients: "cheese,nuts",
-        excludeIngredients: "eggs",
-        instructionsRequired: "true",
-        fillIngredients: "false",
-        addRecipeInformation: "false",
-        addRecipeInstructions: "false",
-        addRecipeNutrition: "false",
-        maxReadyTime: "45",
-        ignorePantry: "true",
-        sort: "max-used-ingredients",
-        offset: "0",
-        number: "10",
+        language: 'en',
+        includeNutrition: 'false',
+        includeTaste: 'false'
       },
       headers: {
-        "x-rapidapi-key": "API_KEY",
-        "x-rapidapi-host":
-          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        'x-rapidapi-key': '43726e468amshea40d69a22978e6p120db7jsn3f76f339f450',
+        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        'Content-Type': 'application/json'
       },
+      data: {
+        title: recipeTitle,
+        servings: 2,
+        ingredients: [
+          '1 lb spaghetti',
+          '3.5 oz pancetta',
+          '2 Tbsps olive oil',
+          '1 egg',
+          '0.5 cup parmesan cheese'
+        ],
+        instructions: 'Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta.'
+      }
     };
 
     try {
@@ -43,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
       resultDiv.innerText = JSON.stringify(response.data, null, 2);
     } catch (error) {
       console.error(error);
+      resultDiv.innerText = "Error analyzing recipe.";
     }
   });
 });
